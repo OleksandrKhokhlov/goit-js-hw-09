34894d1039d29ff3b323fd38ce403acd730bd1b0
OleksandrKhokhlov <118597434+OleksandrKhokhlov@users.noreply.github.com>
2023-06-20 21:33:30 +0300

    HW9 Task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  body: document.body,
+};
 
+let timeIntervalId = null;
+const { startBtn, stopBtn, body } = refs;
+
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
+
+function onStartClick() {
+  startBtn.setAttribute('disabled', 'disabled');
+
+  timeIntervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopClick() {
+    clearInterval(timeIntervalId);
+    startBtn.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
