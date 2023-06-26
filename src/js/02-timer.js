fdc20166b6823d5dd97947cd075dbef2d583095b
OleksandrKhokhlov <118597434+OleksandrKhokhlov@users.noreply.github.com>
2023-06-26 20:50:10 +0300

    HW9

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,19 +1,83 @@
-// import flatpickr from 'flatpickr';
-// import 'flatpickr/dist/flatpickr.min.css';
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-// const options = {
-//   enableTime: true,
-//   time_24hr: true,
-//   defaultDate: new Date(),
-//   minuteIncrement: 1,
-//   onClose(selectedDates) {
-//     console.log(selectedDates[0]);
-//   },
-// };
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  valuesDates: document.querySelectorAll('.value'),
+};
+const { startBtn, valuesDates } = refs;
 
-// flatpickr('#datetime-picker', options);
+const timer = {
+  isActiv: false,
+  selectedTime: 0,
+  start() {
+    if (this.isActiv) {
+      return;
+    }
+    const intervalID = setInterval(() => {
+      const currentTime = Date.now();
+      let ms = timer.selectedTime - currentTime;
 
-const inputRef = document.querySelector('#datetime-picker');
-console.log(1);
-console.log(2);
-console.log(3);
+      const { days, hours, minutes, seconds } = convertMs(ms);
+      valuesDates[0].textContent = `${days}`;
+      valuesDates[1].textContent = `${hours}`;
+      valuesDates[2].textContent = `${minutes}`;
+      valuesDates[3].textContent = `${seconds}`;
+
+      if (Math.trunc(ms / 1000) === 0) {
+        clearInterval(intervalID);
+        startBtn.removeEventListener('click', timer.start);
+      }
+    }, 1000);
+  },
+};
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    startBtn.addEventListener('click', timer.start);
+    currentDate = Date.now();
+    timer.selectedTime = selectedDates[0].getTime();
+    if (currentDate >= timer.selectedTime) {
+      startBtn.setAttribute('disabled', 'disabled');
+      Notify.warning('Please choose a date in the future', {
+        width: '340px',
+        position: 'center-center',
+        cssAnimationStyle: 'zoom',
+      });
+      return;
+    }
+    startBtn.removeAttribute('disabled');
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formRef = document.querySelector('form');
+const dataUser = {};
+
+formRef.addEventListener('submit', onFormSubmit);
+formRef.addEventListener('input', onFormInput);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        // Fulfill
+        res({ position, delay });
+      } else {
+        // Reject
+        rej({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  let delay = Number(dataUser['delay']);
+
+  for (let position = 1; position <= dataUser['amount']; position += 1) {
+    const step = Number(dataUser['step']);
+
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    delay += step;
   }
+}
+
+function onFormInput(evt) {
+  dataUser[evt.target.name] = evt.target.value;
 }
